7bd31ce7b062fa936bbc40f91fb84a6b0fa53d67
N-bred <thehardyboiz1@gmail.com>
2019-08-17 15:26:52 -0500

    add search bar functionality

--- js/index.js
+++ js/index.js
@@ -5,6 +5,15 @@ hamburguerBtn.addEventListener('click', () => {
   docBar.classList.toggle('toggle-bar');
 });
 
+const searchForm = document.querySelector('#search-form');
+const searchBar = document.querySelector('.search-bar');
+
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  find(searchBar.value);
+});
+
 const year = document.getElementById('year');
 const date = new Date();
 year.innerText = date.getFullYear();
